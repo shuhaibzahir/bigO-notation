7dac085003d1b97ac6040eb77061537b6e05e1cb
shuhaib s <shuhaib.s@aot-technologies.com>
2023-01-11 15:29:24 +0530

    O(1)

---
+++ o(1).js
@@ -0,0 +1,11 @@
+/* here we are going to study O(1) 
+1. it the the constant time 
+2. when the input is changed or increased but the operations will heppend one time only
+*/
+
+function addUpto(range){
+    return range * (range +1) /2;
+}
+
+
+/* here you can see the operations will happend one time only */

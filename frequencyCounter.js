9cc13fa4ed0822b540efd275c8973b6b65a0663c
shuhaib s <shuhaib.s@aot-technologies.com>
2023-01-13 17:01:39 +0530

    updated freaqancy counter

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -3,26 +3,32 @@
 function frequencyCounter (array1 , array2){
 
     let obje ={}
+    let obj2 ={}
     if(array1.length !== array2.length){
         return false
     }
 
     for( let item of array1){
-        obje[item * item] =item
+        obje[item] = ++obje[item] || 1
     }
 
     for(let item of array2){
-        if(obje[item] && obje[item] !== true){
-            obje[item]=true
-        }else{
+         obj2[item] = ++obj2[item] || 1
+    }
+    for( let i in obje){
+        if(!obj2[i ** 2]){
             return false
         }
+       if(obje[i] !== obj2[i ** 2]){
+        return false
+       }    
     }
  
+    return true
     // return Object.keys(obje).length === array1.length
 }
 
-console.log(frequencyCounter([1,2,1],[4,4,1]))
+console.log(frequencyCounter([1,2,1],[1,1,1]))
 
 /**
  * {
